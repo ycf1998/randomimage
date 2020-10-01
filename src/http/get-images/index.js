76d182778d710b5ecff37fb7292e1e4c53739bb0
麦尼Money <46522371+ycf1998@users.noreply.github.com>
2020-10-01 14:45:10 +0800

    优化取图算法

--- src/http/get-images/index.js
+++ src/http/get-images/index.js
@@ -13,13 +13,16 @@ let paramsForBaidu = {
 	"ipn": "rt", // 必填参数
 	"word": "", // 关键字
 	"pn": 0, // 起始下标
-	"rn": 0 // 每次请求n张
+	"rn": 0, // 每次请求n张
+	"width": "",
+	"height": "",
 }
-function loadImgsForBaidu(word = '新垣结衣', pageNum = 0, pageSize = 30) {
-	if (pageSize > 100) pageSize = 100;
+function loadImgsForBaidu(word = '新垣结衣', pageNum = 0, pageSize = 100, width = '', height = '') {
 	paramsForBaidu.pn = pageNum;
-	paramsForBaidu.rn = pageSize;
+	paramsForBaidu.rn = 100;
 	paramsForBaidu.word = word;
+	paramsForBaidu.width = width;
+	paramsForBaidu.height = height;
 	let paramStr = formatUrlParams(paramsForBaidu)
 	return fetch(`https://image.baidu.com/search/acjson?${paramStr}`)
 	.then(res => res.json())
@@ -35,7 +38,7 @@ function loadImgsForBaidu(word = '新垣结衣', pageNum = 0, pageSize = 30) {
 			});
 		});
 		return imgs;
-	})
+	}).catch(err => [])
 }  
 
 /**
@@ -51,11 +54,12 @@ let paramsForSouhu = {
 	"cwidth": "", // 宽
 	"cheight": "" //高
 }
-function loadImgsForSouhu(word = '新垣结衣', pageNum = 0, pageSize = 30) {
-	if (pageSize > 100) pageSize = 100;
+function loadImgsForSouhu(word = '新垣结衣', pageNum = 0, pageSize = 100, width = '', height = '') {
 	paramsForSouhu.start = pageNum;
-	paramsForSouhu.xml_len = pageSize;
+	paramsForSouhu.xml_len = 100;
 	paramsForSouhu.query = word;
+	paramsForSouhu.cwidth = width;
+	paramsForSouhu.cheight = height;
 	let paramStr = formatUrlParams(paramsForSouhu)
 	return fetch(`https://pic.sogou.com/api/pic/searchList?${paramStr}`)
 		.then(res => res.json())
@@ -63,26 +67,43 @@ function loadImgsForSouhu(word = '新垣结衣', pageNum = 0, pageSize = 30) {
 			let imgs = [];
 			json.items.forEach(element => {
 				imgs.push({
-					"href": element.oriPicUrl || element.picUrl,
+					"href": element.picUrl,
 					"alt": element.title
 				});
 			});
 			return imgs;
-		})
+		}).catch(err => [])
 }
 
 exports.handler = async function http(req) {
+  /**
+  参数：
+   关键字：word
+   显示n张：size(不超过200)
+   图片宽: width
+   图片高: height
+  **/
   let word;
+  let width;
+  let height;
   let pageSize = 25;
   if (req.queryStringParameters != null) {
 	  word = req.queryStringParameters.word;
-	  pageSize = Math.round(req.queryStringParameters.size / 2) || 25;
+	  width = req.queryStringParameters.width;
+	  height = req.queryStringParameters.height;
+	  pageSize = req.queryStringParameters.size || 20;
   }
   let pageNum = Math.floor(Math.random() * 20 * pageSize);
-  let imgsForBaidu = await loadImgsForBaidu(word, pageNum, pageSize);
-  let imgsForSouhu = await loadImgsForSouhu(word, pageNum, pageSize);
+  // 获取图片
+  let imgsForBaidu = await loadImgsForBaidu(word, pageNum, pageSize, width, height);
+  let imgsForSouhu = await loadImgsForSouhu(word, pageNum, pageSize, width, height);
+  // 处理聚合图片
+  let allImgs = [...imgsForBaidu, ...imgsForSouhu];
+  allImgs = allImgs.filter(img => img.alt.match(word = word || '新垣结衣'))
+  allImgs = allImgs.length > pageSize ? allImgs.slice(0, pageSize) : allImgs;
+
   let data = fs.readFileSync('./index.html')
-  let body = template.render(data.toString(), { result: [...imgsForBaidu, ...imgsForSouhu]  });
+  let body = template.render(data.toString(), { result: allImgs});
   return {
         statusCode: 200,
         headers: {'Content-Type': 'text/html; charset=utf8'},
